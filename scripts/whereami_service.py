5082edb8a28b364b4dda70ebe36e5d444c42d774
Akhilesh <akoo09@seas.upenn.edu>
2019-11-08 11:41:04 -0500

    updated whereami to reply with floor and building

--- scripts/whereami_service.py
+++ scripts/whereami_service.py
@@ -6,14 +6,33 @@ import json
 import rospkg
 import os
 import sys
+import yaml
 import wifi_slam.srv
 import wifi_slam.msg
+
+rospack = rospkg.RosPack()
+package_path = rospack.get_path('wifi_slam')
+config_path = os.path.join(package_path, 'maps', 'whereami_locations.yml')
+with open(config_path, 'r') as f:
+    whereami_cfg = yaml.load(f)
 
 def handle_get_location(req):
 
     loc = predict()
+    loc_building = ''
+    loc_floor = ''
+    flag = 0
 
-    return wifi_slam.srv.GetLocationResponse(loc)
+    buildings = list(whereami_cfg.keys())
+    for building in buildings:
+        floors = list(whereami_cfg[building].keys())
+        for floor in floors:
+            regions = whereami_cfg[building][floor]['regions']
+            if loc in regions:
+                loc_building = building
+                loc_floor = floor
+
+    return wifi_slam.srv.GetLocationResponse(loc_floor, loc_building, loc)
 
 def handle_get_all_prob(req):
 
@@ -28,7 +47,6 @@ def whereami_node():
     s1 = rospy.Service('get_location', wifi_slam.srv.GetLocation, handle_get_location)
     s2 = rospy.Service('get_all_prob', wifi_slam.srv.GetProbability, handle_get_all_prob)
 #    s4 = rospy.Service('get_all_locations', GetAllLocations, handle_get_all_locations)
-
     rospy.spin()
 
 if __name__=="__main__":
